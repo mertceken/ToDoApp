c7f8476612117d1045c43d1598ee2c518ec26630
Mert Çeken <mertceken12@gmail.com>
2020-10-31 16:23:33 +0300

    ToDoApp

---
+++ 20201031_ToDo/20201031_ToDo/Models/TaskList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _20201031_ToDo.Models
+{
+    public class TaskList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string TaskName { get; set; }
+    }
+}
